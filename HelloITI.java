7b4852bfa58a0120784325649a9bad44ce2905f4
Omar <omarmetwally2008@hotmail.com>
2023-12-13 12:48:44 +0200

    modify file

--- HelloITI.java
+++ HelloITI.java
@@ -3,6 +3,7 @@ public class HelloITI
 	
 	void printHelloITI(){
 		System.out.printl("Hello ITI");
+		System.out.printl("Hello conflict");
 	}
 	void printHelloGit(){
 		System.out.printl("Hello Git");
